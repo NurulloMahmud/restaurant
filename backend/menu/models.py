d806876fd48212b7589b3ed29c73869a55aa4695
Nurullo <94496332+NurullohMahmud@users.noreply.github.com>
2024-01-10 17:07:30 -0600

    food model

--- backend/menu/models.py
+++ backend/menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available_qty = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
